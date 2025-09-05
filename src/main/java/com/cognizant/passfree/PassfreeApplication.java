59240f022c72e1870c152a46ee5a07f9f377a86d
Sudhir Gaikwad <ssudhir.gaikwad@gmail.com>
2025-09-05 15:38:32 +0530

    init

---
+++ src/main/java/com/cognizant/passfree/PassfreeApplication.java
@@ -0,0 +1,12 @@
+package com.cognizant.passfree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassfreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassfreeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cognizant/passfree/PassfreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.passfree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassfreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
